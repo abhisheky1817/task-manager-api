8bf118b06703502630500ec63f78c4d2b0d7796a
abhisheky1817 <abhisheky.1817@gmail.com>
2026-02-24 00:40:20 +0530

    setup basic route

--- src/app.js
+++ src/app.js
@@ -1,14 +1,13 @@
 import express from 'express';
 import { PORT } from './config/server.js';
+import taskRoutes from './routes/task.routes.js';
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req,res)=>{
-   res.send("Hello World");
-   res.end();
-});
+app.use('/tasks', taskRoutes);
+
 
 app.listen(PORT, ()=>{
     console.log(`Server is runnging on http://Localhost:${PORT}`);
